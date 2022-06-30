1a23d7fbd312ab4f9b2f4555be6d5c21cf816583
Fabry200 <39920892+Fabry200@users.noreply.github.com>
2022-06-30 18:31:52 +0200

    Update AI_day_and_profit_prevision.py

--- AI_day_and_profit_prevision.py
+++ AI_day_and_profit_prevision.py
@@ -18,16 +18,16 @@ for t in range(200000):
     y_pred = a
     drandom_pred = b
     domani_pred = c
-    # Compute and print loss
+   
     loss = (y_pred - y).sum()
-    # Backprop to compute gradients of a, b, c, d with respect to loss
+    
     grad_y_pred =50*(y_pred - y)
     grad_a = sum(grad_y_pred)
     grad_drandom_pred =50*(drandom_pred - drandom)
     grad_b = grad_drandom_pred.sum()
     grad_domani_pred=50*(y_pred -y)
     grad_c = grad_domani_pred.sum()
-    # Update weights using gradient descent
+
     a -= learning_rate * grad_a
     b -= learning_rate * grad_b
     c -= learning_rate * grad_c
